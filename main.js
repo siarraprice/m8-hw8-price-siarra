af101718e3f644d8118b34213b9bdfaaa8666815
Siarra Price <siarraprice@ufl.edu>
2019-10-13 21:48:13 -0500

    Show services and company submenus on hover

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-document.getElementById("products").addEventListener("mouseover", openMenu);
+//product menu
+document.getElementById("products").addEventListener("mouseover", openMenu1);
 
-function openMenu() {
+function openMenu1() {
   document.getElementById("productsDropdown").classList.toggle("active");
+}
+
+//services menu
+document.getElementById("services").addEventListener("mouseover", openMenu2);
+
+function openMenu2() {
+  document.getElementById("servicesDropdown").classList.toggle("active");
+}
+
+//company menu
+document.getElementById("company").addEventListener("mouseover", openMenu3);
+
+function openMenu3() {
+  document.getElementById("companyDropdown").classList.toggle("active");
 }
