4b8dcb76341b1bc240768946e92244b83f849622
Siarra Price <siarraprice@ufl.edu>
2019-10-14 10:26:46 -0500

    Attempt at coding a hide line of code for the submenu

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 //product menu
 document.getElementById("products").addEventListener("mouseover", openMenu1);
+//document.getElementById("products").addEventListener("mouseout", closeMenu1);
 
 function openMenu1() {
   document.getElementById("productsDropdown").classList.toggle("show");
 }
+
+//function closeMenu1() {
+//  document.getElementById("productsDropdown").classList.toggle("hide");
+//}
+
+
 
 
 
@@ -12,10 +19,14 @@ function openMenu1() {
 
 //services menu
 document.getElementById("services").addEventListener("mouseover", openMenu2);
+//document.getElementById("services").addEventListener("mouseout", closeMenu2);
 
 function openMenu2() {
   document.getElementById("servicesDropdown").classList.toggle("show");
 }
+//function closeMenu2() {
+  //document.getElementById("servicesDropdown").classList.toggle("hide");
+//}
 
 
 
@@ -24,7 +35,11 @@ function openMenu2() {
 
 //company menu
 document.getElementById("company").addEventListener("mouseover", openMenu3);
+//document.getElementById("company").addEventListener("mouseout", closeMenu3);
 
 function openMenu3() {
   document.getElementById("companyDropdown").classList.toggle("show");
 }
+//function closeMenu3() {
+//  document.getElementById("companyDropdown ").classList.toggle("hide");
+//}
